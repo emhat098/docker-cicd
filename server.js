2a403006d2c1eedde591aaabd4786561469b0c59
Em Ha Tuan <emhat098@gmail.com>
2023-03-26 13:01:54 +0700

    init project

---
+++ server.js
@@ -0,0 +1,28 @@
+const ronin = require( 'ronin-server' )
+const database  = require( 'ronin-database' )
+const mocks = require( 'ronin-mocks' )
+
+async function main() {
+    try {
+    await database.connect( process.env.CONNECTIONSTRING )
+    
+    const server = ronin.server({
+            port: process.env.SERVER_PORT
+        })
+
+        server.use( '/', mocks.server( server.Router()) )
+
+        server.use("/hallo", (req, res) => {
+            return res.json({msg: "Hallo world"});
+        })
+
+    const result = await server.start()
+        console.info( result )
+    
+    } catch( error ) {
+        console.error( error )
+    }
+}
+
+main();
+
